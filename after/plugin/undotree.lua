ad247b5ae4062b986e53e2446f29f616bd1df154
CWood-sdf <woodchristophermichael@gmail.com>
2023-07-13 00:24:51 -0400

    clean undo remaps

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,43 +1,30 @@
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-
-vim.keymap.set('n', '<C-z>', function()
-    local count = 1
-    if (vim.v.count == nil) then
+local undo = function(count)
+    if (count == nil) then
         count = 1
-    else
-        count = vim.v.count
     end
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g-", true, true, true), "n", false)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "u", true, true, true), "n", false)
+end
+local redo = function(count)
+    if (count == nil) then
+        count = 1
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "<C-r>", true, true, true), "n", false)
+end
+vim.keymap.set('n', '<C-z>', function()
+    undo(vim.v.count)
 end)
 
 vim.keymap.set('n', '<C-y>', function()
-    local count = 1
-    if (vim.v.count == nil) then
-        count = 1
-    else
-        count = vim.v.count
-    end
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g+", true, true, true), "n", false)
+    redo(vim.v.count)
 end)
 
 vim.keymap.set('i', '<C-z>', function()
-    local count = 1
-    if (vim.v.count == nil) then
-        count = 1
-    else
-        count = vim.v.count
-    end
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g-", true, true, true), "x!", false)
-    vim.cmd('startinsert')
+    vim.cmd('stopinsert')
+    undo(1)
 end)
 
 vim.keymap.set('i', '<C-y>', function()
-    local count = 1
-    if (vim.v.count == nil) then
-        count = 1
-    else
-        count = vim.v.count
-    end
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g+", true, true, true), "x!", false)
-    vim.cmd('startinsert')
+    vim.cmd('stopinsert')
+    redo(1)
 end)
