954b2096f31216b62e28c17041c818ac7fb2d8fb
CWood-sdf <woodchristophermichael@gmail.com>
2023-07-13 00:13:17 -0400

    update C-z and y to use counts

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,9 +1,21 @@
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
 vim.keymap.set('n', '<C-z>', function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("g-", true, true, true), "n", true)
+    local count = 1
+    if (vim.v.count == nil) then
+        count = 1
+    else
+        count = vim.v.count
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g-", true, true, true), "n", false)
 end)
 
 vim.keymap.set('n', '<C-y>', function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("g+", true, true, true), "n", true)
+    local count = 1
+    if (vim.v.count == nil) then
+        count = 1
+    else
+        count = vim.v.count
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g+", true, true, true), "n", false)
 end)
