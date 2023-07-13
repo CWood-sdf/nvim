58377be4ba957fd8f3f7aaea0a8f353d2e2e1c41
CWood-sdf <woodchristophermichael@gmail.com>
2023-07-13 00:17:00 -0400

    yo now C-z works for insert only once

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -19,3 +19,23 @@ vim.keymap.set('n', '<C-y>', function()
     end
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g+", true, true, true), "n", false)
 end)
+
+vim.keymap.set('i', '<C-z>', function()
+    local count = 1
+    if (vim.v.count == nil) then
+        count = 1
+    else
+        count = vim.v.count
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g-", true, true, true), "x!", false)
+end)
+
+vim.keymap.set('i', '<C-y>', function()
+    local count = 1
+    if (vim.v.count == nil) then
+        count = 1
+    else
+        count = vim.v.count
+    end
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(count .. "g+", true, true, true), "n", false)
+end)
