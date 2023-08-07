5ba7427213cdfe0b0964a39d4209d73f43528a02
CWood-sdf <woodchristophermichael@gmail.com>
2023-08-07 00:22:25 -0400

    readd mode component

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -83,38 +83,38 @@ ins_left {
     padding = { left = 0, right = 0 }, -- We don't need space before this
 }
 
--- ins_left {
---     -- mode component
---     function()
---         return ''
---     end,
---     color = function()
---         -- auto change color according to neovims mode
---         local mode_color = {
---             n = colors.red,
---             i = colors.green,
---             v = colors.blue,
---             [''] = colors.blue,
---             V = colors.blue,
---             c = colors.magenta,
---             no = colors.red,
---             s = colors.orange,
---             S = colors.orange,
---             ic = colors.yellow,
---             R = colors.violet,
---             Rv = colors.violet,
---             cv = colors.red,
---             ce = colors.red,
---             r = colors.cyan,
---             rm = colors.cyan,
---             ['r?'] = colors.cyan,
---             ['!'] = colors.red,
---             t = colors.red,
---         }
---         return { fg = mode_color[vim.fn.mode()] }
---     end,
---     padding = { right = 1 },
--- }
+ins_left {
+    -- mode component
+    function()
+        return ''
+    end,
+    color = function()
+        -- auto change color according to neovims mode
+        local mode_color = {
+            n = colors.red,
+            i = colors.green,
+            v = colors.blue,
+            [''] = colors.blue,
+            V = colors.blue,
+            c = colors.magenta,
+            no = colors.red,
+            s = colors.orange,
+            S = colors.orange,
+            ic = colors.yellow,
+            R = colors.violet,
+            Rv = colors.violet,
+            cv = colors.red,
+            ce = colors.red,
+            r = colors.cyan,
+            rm = colors.cyan,
+            ['r?'] = colors.cyan,
+            ['!'] = colors.red,
+            t = colors.red,
+        }
+        return { fg = mode_color[vim.fn.mode()] }
+    end,
+    padding = { right = 1, left = 1 },
+}
 
 ins_left {
     -- filesize component
