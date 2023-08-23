16ac9483a6004f04b8c03b51839da806efeb2bf0
CWood-sdf <woodchristophermichael@gmail.com>
2023-08-22 23:14:27 -0400

    clean up the yanking and pasting stuff

--- lua/stuff/remap.lua
+++ lua/stuff/remap.lua
@@ -2,7 +2,6 @@ local wk = require("stuff.wkutils")
 
 vim.g.mapleader = " ";
 
--- allows matched chars to be deleted without removing inners
 wk.makeGroup("n", "do", "Over", function(remap)
     remap("%", "match", function()
         local charUnderCursor = vim.fn.getline("."):sub(vim.fn.col("."), vim.fn.col("."))
@@ -43,7 +42,7 @@ end)
 -- so i just made it so that enter is control enter
 -- idk wtf happened
 vim.keymap.set("i", "<CR>", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-CR>", true, true, true), "i", true)
+    wk.feedKeys("<C-CR>", "i")
 end)
 
 -- move stuff up and down
@@ -56,7 +55,7 @@ wk.remapNoGroup('v', '<leader>k', 'Move selected up', function()
         count = 1
     end
     count = count + 1
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":m '>-" .. count .. "<CR>", true, true, true), "v", true)
+    wk.feedKeys(":m '>-" .. count .. "<CR>", "v")
 end)
 wk.remapNoGroup('v', '<leader>j', 'Move selected down', function()
     local count = 0
@@ -66,36 +65,18 @@ wk.remapNoGroup('v', '<leader>j', 'Move selected down', function()
     if (vim.v.count == 0) then
         count = 1
     end
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(":m '>+" .. count .. "<CR>", true, true, true), "v", true)
+    wk.feedKeys(":m '>+" .. count .. "<CR>", "v")
 end)
 
 -- useful visual mode stuff
 wk.remapNoGroup("v", "D", "Void delete", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"_d", true, true, true), "v", true)
+    wk.feedKeys("\"_d", "n")
 end)
 wk.remapNoGroup("v", "Y", "Yank reg x", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"xy", true, true, true), "v", true)
-end)
-wk.remapNoGroup("o", "Y", "Yank reg x", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"xy", true, true, true), "o", true)
-end)
-wk.remapNoGroup("x", "Y", "Yank reg x", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"xy", true, true, true), "x", true)
+    wk.feedKeys("\"xy", "n")
 end)
 wk.remapNoGroup("v", "P", "Paste reg x", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"xp", true, true, true), "v", true)
-end)
-wk.remapNoGroup("x", "D", "Void delete", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"_d", true, true, true), "x", true)
-end)
-wk.remapNoGroup("x", "P", "Paste reg x", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"xp", true, true, true), "x", true)
-end)
-wk.remapNoGroup("o", "D", "Void delete", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"_d", true, true, true), "o", true)
-end)
-wk.remapNoGroup("o", "P", "Paste reg x", function()
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("\"xp", true, true, true), "o", true)
+    wk.feedKeys("\"xp", "n")
 end)
 
 
