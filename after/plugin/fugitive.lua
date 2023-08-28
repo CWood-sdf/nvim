703bb0ecce2a46edc72f3ac908e30791340fc2c4
CWood-sdf <woodchristophermichael@gmail.com>
2023-08-27 23:22:03 -0400

    yo add some git remaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,6 +2,15 @@ local wk = require("stuff.wkutils");
 
 wk.makeGroup("n", "<leader>g", "[G]it", function(remap)
     remap("s", "[S]tatus", vim.cmd.Git);
+    remap("p", "[P]ush", function()
+        vim.cmd("Git push")
+    end);
+    remap("a", "[A]dd all", function()
+        vim.cmd("Git add .")
+    end);
+    remap("c", "[C]ommit", function()
+        vim.cmd("Git commit")
+    end);
 end)
 
 wk.writeBuf()
